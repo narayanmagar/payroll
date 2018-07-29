7739a3056f5d790e84e2a651a643f94d661bda70
narayanmagar <thmgrnarayan@gmail.com>
2018-07-29 16:57:45 +0545

    models and views added

--- payroll/scale/views.py
+++ payroll/scale/views.py
@@ -3,8 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Scale
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    scale = Scale.objects.all()
+    return render(request, 'home.html', {'scale': scale})
