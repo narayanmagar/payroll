bc3b6ed6f7a74539640cc4822c009440f8c42e9a
narayanmagar <thmgrnarayan@gmail.com>
2018-07-30 16:50:58 +0545

    another app start

--- payroll/payroll/settings.py
+++ payroll/payroll/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scale',
+    'widget_tweaks',
+    'accounts',
+    'boards',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGOUT_REDIRECT_URL = 'home'
+
+LOGIN_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+LOGIN_URL = 'login'
