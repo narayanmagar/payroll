9d96ecff9ae5bbcae6024dfd9c29deb8b22d2e2c
narayanmagar <thmgrnarayan@gmail.com>
2018-07-31 14:38:16 +0545

    work on the post and comments reply

--- payroll/payroll/settings.py
+++ payroll/payroll/settings.py
@@ -128,8 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-LOGIN_REDIRECT_URL = 'home'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-LOGIN_URL = 'login'
