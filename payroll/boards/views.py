658eb56f8b0b903e4d85ff4d1c08a7d6ed82067a
narayanmagar <thmgrnarayan@gmail.com>
2018-07-31 16:36:40 +0545

    final change in views

--- payroll/boards/views.py
+++ payroll/boards/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Count
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import UpdateView, ListView
 from django.utils import timezone
@@ -16,38 +15,10 @@ class BoardListView(ListView):
     template_name = 'home.html'
 
 
-class TopicListView(ListView):
-    model = Topic
-    context_object_name = 'topics'
-    template_name = 'topics.html'
-    paginate_by = 20
-
-    def get_context_data(self, **kwargs):
-        kwargs['board'] = self.board
-        return super().get_context_data(**kwargs)
-
-    def get_queryset(self):
-        self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
-        queryset = self.board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
-        return queryset
-
-
-class PostListView(ListView):
-    model = Post
-    context_object_name = 'posts'
-    template_name = 'topic_posts.html'
-    paginate_by = 20
-
-    def get_context_data(self, **kwargs):
-        self.topic.views += 1
-        self.topic.save()
-        kwargs['topic'] = self.topic
-        return super().get_context_data(**kwargs)
-
-    def get_queryset(self):
-        self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
-        queryset = self.topic.posts.order_by('created_at')
-        return queryset
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 
 @login_required
@@ -69,6 +40,13 @@ def new_topic(request, pk):
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    topic.views += 1
+    topic.save()
+    return render(request, 'topic_posts.html', {'topic': topic})
 
 
 @login_required
