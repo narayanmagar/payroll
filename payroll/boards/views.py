5ff12d86d67112b2941c0af461d74b2aea90bc0c
narayanmagar <thmgrnarayan@gmail.com>
2018-08-01 14:52:44 +0545

    add dynamic posts

--- payroll/boards/views.py
+++ payroll/boards/views.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.http import Http404
-
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Board
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import NewTopicForm
+from .models import Board, Topic, Post
 
 def home(request):
     boards = Board.objects.all()
@@ -13,3 +11,23 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get the currently logged in user
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
